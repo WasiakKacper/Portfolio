5c382f3f1b334d51cad372572c13691035d75ee4
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-03-03 21:43:12 +0100

    Adding navigation

--- main.js
+++ main.js
@@ -7,10 +7,49 @@ window.addEventListener('scroll', ()=>{
     if(value > 500){
         arrowUp.classList.add("active");
     }
-
-    if(value == 0){
+    else{
         arrowUp.classList.remove("active");
     }
+
+    if(value > 1800){
+        arrowDown.classList.remove("active");
+    }
+    else{
+        arrowDown.classList.add("active");
+    }
 });
+
+/* Navigation */
+arrowDown.addEventListener('click', ()=>{
+    if(window.screenY >= 0 && window.scrollY < 980){
+            window.scrollTo({
+                top: 980,
+                behavior: "smooth"
+            })
+        }
+    else if(window.scrollY >= 980){
+        window.scrollTo({
+            top: 1900,
+            behavior: "smooth"
+        })
+    }
+});
+
+arrowUp.addEventListener('click', ()=>{
+    if(window.scrollY <= 980 && window.scrollY < 1400){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+    else if(window.scrollY <= 1900){
+        window.scrollTo({
+            top: 980,
+            behavior: "smooth"
+        })
+    }
+});
+
+
 
 
