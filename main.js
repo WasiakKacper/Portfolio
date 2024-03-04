aca0965872c191ee66185f7f4b18f77857e9291f
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-03-04 12:19:46 +0100

    New animations

--- main.js
+++ main.js
@@ -19,36 +19,82 @@ window.addEventListener('scroll', ()=>{
     }
 });
 
-/* Navigation */
+/* NAVIGATION */
 arrowDown.addEventListener('click', ()=>{
-    if(window.screenY >= 0 && window.scrollY < 980){
+    let value = window.scrollY;
+
+    if(value >= 0 && value < 980){
             window.scrollTo({
                 top: 980,
                 behavior: "smooth"
             })
         }
-    else if(window.scrollY >= 980){
+    else if(value >= 980){
         window.scrollTo({
-            top: 1900,
+            top: 2250,
             behavior: "smooth"
         })
     }
 });
 
 arrowUp.addEventListener('click', ()=>{
-    if(window.scrollY <= 980 && window.scrollY < 1400){
+    let value = window.scrollY;
+
+    if(value <= 980 && value < 1400){
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         })
     }
-    else if(window.scrollY <= 1900){
+    else if(value <= 2250){
         window.scrollTo({
             top: 980,
             behavior: "smooth"
         })
     }
 });
+
+/* LOADING */
+function sleep(ms){
+    return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+const text = "Kacper Wasiak";
+const el = document.getElementById("header");
+
+const write = async ()=>{
+    for(let i = 0; i < text.length; i++){
+        el.innerText = text.substring(0, i + 1);
+        await sleep(100)
+    }
+}
+write()
+
+const subText = "Frontend developer"
+const h2 = document.getElementById("underHeader")
+
+const writeLower = async ()=>{
+    await sleep(1300)
+    for(let i = 0; i < subText.length; i++){
+        h2.innerText = subText.substring(0, i + 1);
+        await sleep(50)
+    }
+}
+writeLower()
+
+/* PARALAX ANIMATIONS */
+ const portfolioTile = document.getElementById("portfolio");
+ const weatherTile = document.getElementById("weather");
+ const todolistTile = document.getElementById("todolist");
+
+function showProject(){
+    let value = window.scrollY;
+    if(value >= 980){
+        portfolioTile.classList.add("active");
+        weatherTile.classList.add("active");
+        todolistTile.classList.add("active");
+    }
+ }
 
 
 
