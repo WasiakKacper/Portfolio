1989610255c4a367614c690a742daf161be31ec9
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-03-29 14:54:52 +0100

    Working webstie navigation

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const writeLower = async ()=>{
 writeLower()
 
 /* PARALAX EVENTS */
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', async ()=>{
     let value = window.scrollY;
     /* show projects */
 
@@ -75,180 +75,18 @@ window.addEventListener('scroll', ()=>{
         aboutTextContent.classList.add("active");
         aboutImage.classList.add("active");
     }
-});
 
-/* NAVIGATION */
-const arrowUp = document.getElementById("arrow-up");
-const arrowDown = document.getElementById("arrow-down");
+    /* show contact page */
+    if(value >= 2700){
+        const paragraph1 = document.getElementById("p1");
+        const paragraph2 = document.getElementById("p2");
 
-/* show arrows */
-window.addEventListener('DOMContentLoaded', ()=>{
-    arrowDown.classList.add("active");
-})
-window.addEventListener('scroll', ()=>{
-    let value = window.scrollY;
-    let viewWidth = document.documentElement.clientWidth;
-
-    if(viewWidth < 980){
-        if(value >= 0 && value <= 4000){
-            arrowDown.classList.add("active");
-        }
-        else{
-            arrowDown.classList.remove("active");
+        function sleep(ms){
+            return new Promise((resolve) => setTimeout(resolve, ms));
         }
 
-        if(value > 500){
-            arrowUp.classList.add("active");
-        }
-        else{
-            arrowUp.classList.remove("active");
-        }
-    }
-    else{
-        if(value >= 0 && value <= 2400){
-            arrowDown.classList.add("active");
-        }
-        else{
-            arrowDown.classList.remove("active");
-        }
-
-        if(value > 500){
-            arrowUp.classList.add("active");
-        }
-        else{
-            arrowUp.classList.remove("active");
-        }
-    }
-});
-
-/* scrolling to each section */
-
-arrowDown.addEventListener('click', ()=>{
-    let value = window.scrollY;
-    let viewWidth = document.documentElement.clientWidth;
-
-    if(viewWidth < 980){
-        /* On mobile */
-        if(value >= 0 && value < 980){
-            const projects = document.getElementById("projects").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: projects,
-                    behavior: "smooth"
-                }
-            )
-        }
-        else if(value >= 980 && value < 2500){
-            const about = document.getElementById("about").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: about,
-                    behavior: "smooth"
-                }
-            )
-        }
-        else{
-            const contact = document.getElementById("contact").offsetTop;
-
-            window.scrollTo({
-                top: contact,
-                behavior: "smooth"
-            })
-        }
-    }
-    else{
-        /* On desktop */
-        if(value >= 0 && value < 980){
-            const projects = document.getElementById("projects").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: projects,
-                    behavior: "smooth"
-                }
-            )
-        }
-        else if(value >= 980 && value < 2000){
-            const about = document.getElementById("about").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: about,
-                    behavior: "smooth"
-                }
-            )
-        }
-        else if(value > 2000){
-            const contact = document.getElementById("contact").offsetTop;
-
-            window.scrollTo({
-                top: contact,
-                behavior: "smooth"
-            })
-        }
-    }
-});
-
-arrowUp.addEventListener('click', ()=>{
-    let value = window.scrollY;
-    let viewWidth = document.documentElement.clientWidth;
-
-    if(viewWidth < 980){
-        /* On mobile */
-        if(value >= 980 && value < 2000){
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            })
-        }
-        else if(value >= 3500 && value < 4000){
-            const projects = document.getElementById("projects").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: projects,
-                    behavior: "smooth"
-                })
-        }
-        else{
-            const about = document.getElementById("about").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: about,
-                    behavior: "smooth"
-                }
-            )
-        }
-    }
-    else{
-        /* On desktop */
-        if(value >= 980 && value < 2000){
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            })
-        }
-        else if(value >= 2000 && value < 3000){
-            const projects = document.getElementById("projects").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: projects,
-                    behavior: "smooth"
-                })
-        }
-        else{
-            const about = document.getElementById("about").offsetTop;
-            
-            window.scrollTo(
-                {
-                    top: about,
-                    behavior: "smooth"
-                }
-            )
-        }
+        paragraph1.classList.add("active");
+        await sleep(200);
+        paragraph2.classList.add("active");
     }
 });
---
+++ navigation.js
@@ -0,0 +1,91 @@
+/* NAVIGATION */
+const arrowUp = document.getElementById("arrow-up");
+const arrowDown = document.getElementById("arrow-down");
+
+/* show arrows */
+window.addEventListener('scroll',()=>{
+    let value = window.scrollY;
+
+    const home = document.getElementById("homePage").offsetTop;
+    const projects = document.getElementById("projects").offsetTop;
+    const about = document.getElementById("about").offsetTop;
+    
+    if(value > home){
+        arrowUp.classList.add('active');
+    }
+
+    if(value > about + 200){
+        arrowDown.classList.remove('active');
+    }
+
+    if(value < about + 200){
+        arrowDown.classList.add('active');
+
+    }
+
+    if(value < projects - 500){
+        arrowUp.classList.remove('active');
+    }
+});
+
+/* SCROLLING TO EACH SECTION */
+/* scroll down */
+arrowDown.addEventListener('click', ()=>{
+    let value = window.scrollY;
+
+    const home = document.getElementById("homePage").offsetTop;
+    const projects = document.getElementById("projects").offsetTop;
+    const about = document.getElementById("about").offsetTop;
+    const contact = document.getElementById("contact").offsetTop;
+
+    if(value >= 0 && value <= projects){
+        window.scrollTo({
+            top: projects,
+            behavior: "smooth"
+        });
+    }
+
+    if(value >= projects && value <= about){
+        window.scrollTo({
+            top: about,
+            behavior: "smooth"
+        });
+    }
+
+    if(value >= about && value <= contact){
+        window.scrollTo({
+            top: contact,
+            behavior: "smooth"
+        });
+    }
+});
+
+/* scroll up */
+arrowUp.addEventListener('click', ()=>{
+    let value = window.scrollY;
+
+    const projects = document.getElementById("projects").offsetTop;
+    const about = document.getElementById("about").offsetTop;
+    const contact = document.getElementById("contact").offsetTop;
+
+    if(value > 0 && value <= projects){
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    if(value > projects && value <= about){
+        window.scrollTo({
+            top: projects,
+            behavior: "smooth"
+        });
+    }
+
+    if(value > about && value <= contact){
+        window.scrollTo({
+            top: about,
+            behavior: "smooth"  
+        });
+    }
+});
