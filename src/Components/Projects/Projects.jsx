236c90dbf757d4e07546f8d7871dd6ece1d3f1d4
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-01-16 05:23:09 +0100

    feat(Projects): Add Projects componen and add styles to it.

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,35 +1,86 @@
 import "./Projects.css";
 import { motion } from "motion/react";
+import PropTypes from "prop-types";
+
+const descriptionArr = [
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi suscipit quod cupiditate vitae amet, quam quaerat enim. Officiis culpa voluptatem adipisci maiores libero quos, tempore, ea amet corporis, vitae veniam.",
+
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi suscipit quod cupiditate vitae amet, quam quaerat enim. Officiis culpa voluptatem adipisci maiores libero quos, tempore, ea amet corporis, vitae veniam.",
+
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi suscipit quod cupiditate vitae amet, quam quaerat enim. Officiis culpa voluptatem adipisci maiores libero quos, tempore, ea amet corporis, vitae veniam.",
+];
+
+const urlArr = [
+  "./Images/Portfolio.png",
+  "./Images/weather.png",
+  "./Images/todolist.png",
+];
 
 const Projects = () => {
-  const ProjectCard = () => {
+  const ProjectCard = (props) => {
     return (
-      <motion.article>
-        <img src="" alt="" />
+      <motion.article
+        className="project"
+        initial={{ transform: "translateY(500px)", opacity: 0 }}
+        whileInView={{ transform: "translateY(0px)", opacity: 1 }}
+        transition={{ duration: 0.6, type: "spring", bounce: 0.4 }}
+      >
+        <img src={props.src} alt="" />
         <div className="wrapper">
-          <h3>Lorem ipsum</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
-            veritatis eius sint, molestiae laboriosam alias rem consequuntur ea
-            nobis eum cum temporibus, totam ipsa, voluptate labore sit
-            consectetur? Molestias, ratione!
-          </p>
+          <h3>{props.name}</h3>
+          <p>{props.description}</p>
           <nav>
-            <motion.a>Zobacz stronę</motion.a>
-            <motion.a>Zobacz kod</motion.a>
+            <motion.a
+              initial={{ transform: "translateY(0px)" }}
+              whileHover={{ transform: "translateY(-20px)" }}
+              transition={{ type: "spring", bounce: 0.4 }}
+            >
+              Zobacz stronę
+            </motion.a>
+            <motion.a
+              initial={{ transform: "translateY(0px)" }}
+              whileHover={{ transform: "translateY(-10px)" }}
+              transition={{ type: "spring", bounce: 0.4 }}
+            >
+              Zobacz kod
+            </motion.a>
           </nav>
         </div>
       </motion.article>
     );
   };
 
+  ProjectCard.propTypes = {
+    src: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+  };
+
   return (
-    <main className="projects">
-      <h1>Projekty</h1>
+    <main id="projects">
+      <motion.h1
+        initial={{ transform: "translateY(500px)", opacity: 0 }}
+        whileInView={{ transform: "translateY(0px)", opacity: 1 }}
+        transition={{ duration: 0.6, type: "spring", bounce: 0.4 }}
+      >
+        Projekty
+      </motion.h1>
       <section>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          src={urlArr[0]}
+          name="Portfolio"
+          description={descriptionArr[0]}
+        />
+        <ProjectCard
+          src={urlArr[1]}
+          name="Weather"
+          description={descriptionArr[1]}
+        />
+        <ProjectCard
+          src={urlArr[2]}
+          name="ToDoList"
+          description={descriptionArr[2]}
+        />
       </section>
     </main>
   );
