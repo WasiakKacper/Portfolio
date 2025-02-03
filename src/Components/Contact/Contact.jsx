f4ba91feab2109d0c5ccb08f1b5de936e2aea76b
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-02-03 11:05:49 +0100

    feat(Contact): Add Contact component structure

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+function Contact() {
+  return (
+    <>
+      <form action="">
+        <div>
+          <input type="text" />
+          <input type="email" name="" id="" />
+        </div>
+        <textarea name="" id=""></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default Contact;
