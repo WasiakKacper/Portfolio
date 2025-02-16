4a62294747dcd69515b21d01e5eb747125b07f1c
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-02-17 00:01:00 +0100

    feat: Add loading for website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Home from "./Components/Home/Home.jsx";
 import "./App.css";
-
+import { useState, useEffect } from "react";
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setIsLoaded(true);
+    });
+  }, []);
+
   return (
     <>
+      <section className={`loaderWrapper ${!isLoaded ? "active" : "unactive"}`}>
+        <div className="loader"></div>
+      </section>
       <Home />
     </>
   );
