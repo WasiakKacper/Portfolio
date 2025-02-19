9a2071c46e4519da13138c77ce740c5e7beb2426
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-02-19 05:15:51 +0100

    style(Home): Add hover effect for techstack images

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,16 +7,28 @@ function Home() {
       <h3>Frontend developer</h3>
       <section id="wrapper">
         <div className="blocks">
-          <img src="/Images/HTML_IMAGE.svg" alt="HTML" />
+          <img className="block" src="/Images/HTML2_IMAGE.svg" alt="HTML" />
+          <img className="block" src="/Images/HTML_IMAGE.svg" alt="HTML" />
         </div>
         <div className="blocks">
-          <img src="/Images/CSS_IMAGE.svg" alt="CSS" />
+          <img className="block" src="/Images/CSS2_IMAGE.svg" alt="CSS" />
+          <img className="block" src="/Images/CSS_IMAGE.svg" alt="CSS" />
         </div>
         <div className="blocks">
-          <img src="/Images/JavaScript_IMAGE.svg" alt="JavaScript" />
+          <img
+            className="block"
+            src="/Images/JavaScript2_IMAGE.svg"
+            alt="JavaScript"
+          />
+          <img
+            className="block"
+            src="/Images/JavaScript_IMAGE.svg"
+            alt="JavaScript"
+          />
         </div>
         <div className="blocks">
-          <img src="/Images/React_IMAGE.svg" alt="React" />
+          <img className="block" src="/Images/React2_IMAGE.svg" alt="React" />
+          <img className="block" src="/Images/React_IMAGE.svg" alt="React" />
         </div>
       </section>
     </main>
