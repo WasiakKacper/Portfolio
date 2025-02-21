e1ee38f4774685743aa5044f458db648957895a5
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-02-21 11:55:53 +0100

    Feat(Home): Add Styles.js file with styles for tailwind.css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,57 +1,49 @@
 import "./Home.css";
+import Styles from "./Styles.js";
 
 function Home() {
   return (
     <main
       id="home"
-      className="w-full h-full flex flex-col items-center justify-center !important"
+      className="w-full h-full flex flex-col items-center justify-center"
     >
+      {/* HEADING */}
       <h1 className="w-120 md:w-200 bg-(--dark) text-5xl md:text-9xl font-[Jost] text-(--white) text-center tracking-[4px] block">
         KACPER WASIAK
       </h1>
       <h3 className="text-center text-[90px]">Frontend developer</h3>
+
+      {/* TECH STACK */}
       <section
         id="wrapper"
         className="flex flex-col md:flex-row w-full items-center justify-center"
       >
-        <div className="left flex flex-col md:flex-row justify-around">
-          <div className="blocks">
-            {/*             <img className="block" src="/Images/HTML2_IMAGE.svg" alt="HTML" /> */}
-            <img
-              className="relative after:absolute after:content['HTML'] after:text-(--dark) hover:brightness-50 block"
-              src="/Images/HTML_IMAGE.svg"
-              alt="Tech stack: HTML"
-            />
+        {/* LEFT */}
+        <div className="left">
+          {/* HTML */}
+          <div className={`${Styles.techStackBlock} after:content-['HTML']`}>
+            <img src="/Images/HTML_IMAGE.svg" alt="Tech stack: HTML" />
           </div>
-          <div className="blocks hover:brightness-50 transition duration-300 ease">
-            {/*             <img className="block" src="/Images/CSS2_IMAGE.svg" alt="CSS" /> */}
-            <img
-              className="block hover:brightness-50 transition duration-300 ease"
-              src="/Images/CSS_IMAGE.svg"
-              alt="Tech stack: CSS"
-            />
+          {/* CSS */}
+          <div className={`${Styles.techStackBlock} after:content-['CSS']`}>
+            <img src="/Images/CSS_IMAGE.svg" alt="Tech stack: CSS" />
           </div>
         </div>
-        <div className="right flex flex-col md:flex-row">
-          <div className="blocks">
-            {/*             <img
-              className="block"
-              src="/Images/JavaScript2_IMAGE.svg"
-              alt="JavaScript"
-            /> */}
+
+        {/* RIGHT */}
+        <div className="right">
+          {/* JAVASCRIPT */}
+          <div
+            className={`${Styles.techStackBlock} after:content-['JavaScript']`}
+          >
             <img
-              className="block hover:brightness-50 transition duration-300 ease"
               src="/Images/JavaScript_IMAGE.svg"
               alt="Tech stack: JavaScript"
             />
           </div>
-          <div className="blocks">
-            {/*             <img className="block" src="/Images/React2_IMAGE.svg" alt="React" /> */}
-            <img
-              className="block hover:brightness-50 transition duration-300 ease"
-              src="/Images/React_IMAGE.svg"
-              alt="Tech stack: React"
-            />
+          {/* REACT */}
+          <div className={`${Styles.techStackBlock} after:content-['React']`}>
+            <img src="/Images/React_IMAGE.svg" alt="Tech stack: React" />
           </div>
         </div>
       </section>
---
+++ src/Components/Home/Styles.js
@@ -0,0 +1,6 @@
+const Styles = {
+  techStackBlock:
+    "blocksrelative after:text-5xl after:text-(--white) after:font-bold after:font-[Inter] after: after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:hidden after:transition after:duration-300 after:ease transition duration-300 ease hover:after:block hover:brightness-50 hover:after:brightness-100",
+};
+
+export default Styles;
