f4bed4c88f689057800f3ab8e6c728d1258ac24c
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-02-23 07:49:13 +0100

    Style: End styling nav component with tailwind.css

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,18 +1,28 @@
+import Styles from "../../Styles.js";
+
 function Nav() {
   return (
     <nav className="w-[60%] fixed bottom-0 left-1/2 -translate-x-1/2 bg-(--dark) rounded-t-3xl">
-      <ul className="w-full h-15 text-(--white) text-2xl lg:text-4xl font-[Joan] flex flex-row justify-center gap-3 lg:gap-5 p-3 tracking-[3px] lg:tracking-[4px]">
+      <ul className="w-full h-15 text-(--white) text-2xl lg:text-4xl font-[Joan] flex flex-row justify-center lg:gap-5 p-3 tracking-[3px] lg:tracking-[4px]">
         <li>
-          <a href="#">Home</a>
+          <a href="#" className={Styles.aTag}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="#">Projekty</a>
+          <a href="#" className={Styles.aTag}>
+            Projekty
+          </a>
         </li>
         <li>
-          <a href="#">O mnie</a>
+          <a href="#" className={Styles.aTag}>
+            O mnie
+          </a>
         </li>
         <li>
-          <a href="#">Kontakt</a>
+          <a href="#" className={Styles.aTag}>
+            Kontakt
+          </a>
         </li>
       </ul>
     </nav>
