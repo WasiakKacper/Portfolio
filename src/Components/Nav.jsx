b619de2d69a5d6a6eba66c1f2c99a5156cb1f50a
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-03-04 11:31:39 +0100

    style(Nav): Add animation on load for navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,24 @@
 import Styles from "../Styles.js";
+import { useState, useEffect } from "react";
+import { motion } from "motion/react";
 
 function Nav() {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsActive(true);
+    }, 600);
+  }, []);
+
   return (
-    <nav className="w-[60%] fixed bottom-0 left-1/2 -translate-x-1/2 bg-(--dark) rounded-t-3xl z-10000">
+    <motion.nav
+      className="w-[60%] fixed bottom-0 left-1/2 -translate-x-1/2 bg-(--dark) rounded-t-3xl z-10000"
+      initial={{ scaleX: 0 }}
+      whileInView={isActive ? { scaleX: 1 } : {}}
+      transition={{ duration: 0.4, delay: 2.3 }}
+      viewport={{ once: true }}
+    >
       <ul className="w-full h-15 text-(--white) text-2xl lg:text-4xl font-[Joan] flex flex-row justify-center lg:gap-5 p-3 tracking-[3px] lg:tracking-[4px]">
         <li>
           <a href="#" className={Styles.aTag}>
@@ -25,7 +41,7 @@ function Nav() {
           </a>
         </li>
       </ul>
-    </nav>
+    </motion.nav>
   );
 }
 
