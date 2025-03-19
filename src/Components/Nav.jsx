694782f02405e74750462f6b253bf3d615cbda0a
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-03-19 11:35:52 +0100

    Style(Nav): Fix Nav component style.

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,26 +1,17 @@
 import Styles from "../Styles.js";
-import { useState, useEffect } from "react";
 import { motion } from "motion/react";
 import { Link } from "react-scroll";
 
 function Nav() {
-  const [isActive, setIsActive] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsActive(true);
-    }, 1600);
-  }, []);
-
   return (
     <motion.nav
-      className="w-[99%] md:w-[60%] fixed bottom-0 left-1/2 -translate-x-1/2 bg-(--dark) rounded-t-3xl z-10000"
+      className="w-[99%] md:w-[70%] fixed bottom-0 left-1/2 -translate-x-1/2 bg-(--dark) rounded-t-3xl z-10000"
       initial={{ scaleY: 0, height: 0 }}
-      whileInView={isActive ? { scaleY: 1, height: "auto" } : {}}
+      whileInView={{ scaleY: 1, height: "auto" }}
       transition={{ duration: 0.4, delay: 2.3 }}
       viewport={{ once: true }}
     >
-      <ul className="w-full h-15 text-(--white) text-2xl lg:text-4xl font-[Joan] flex flex-row justify-center lg:gap-5 p-3 tracking-[3px] lg:tracking-[4px]">
+      <ul className="w-full h-12 lg:h-15 text-(--white) text-xl lg:text-2xl font-[Joan] flex flex-row justify-center lg:gap-2 p-3 tracking-[3px] lg:tracking-[4px]">
         <li>
           <Link
             to="/"
--- src/Styles.js
+++ src/Styles.js
@@ -2,7 +2,7 @@ const Styles = {
   techStackBlockWrapper: "flex flex-col md:flex-row gap-4 md:gap-7",
   techStackBlock: "group relative *:transition *:duration-300",
   span: "text-4xl md:5xl text-(--white) font-bold font-[Inter] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block",
-  aTag: "block w-[100%] mx-1 transition duration-150 ease hover:opacity-80 cursor-pointer text-[18px] md:text-2xl lg:text-4xl",
+  aTag: "block w-[100%] h-100% mx-1 transition duration-150 ease hover:opacity-80 cursor-pointer text-[18px] md:text-2xl lg:text-4xl",
   inputText: "bg-(--white) p-[1%] pl-[2%]",
   inputTextSpan: "text-2xl text-(--white) ml-2 text-2xl text-(--white) ml-2",
 };
