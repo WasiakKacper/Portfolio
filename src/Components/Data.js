dee93ce8ffef478109124554b1d14b3176bffca2
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-03-19 11:54:33 +0100

    Fix(Data.js): Change project description.

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -25,7 +25,7 @@ export const projectsData = [
   {
     url: "/Images/Portfolio.png",
     description:
-      "Moje portfolio to nowoczesna i przejrzysta strona, która w prosty sposób prezentuje moje projekty. Stworzyłem ją przy użyciu Reacta, Motion, Tailwind CSS, React Scroll oraz EmailJS, co zapewnia płynne animacje, elegancki wygląd i intuicyjną nawigację.Strona zawiera navbar, umożliwiający szybkie przechodzenie między sekcjami, oraz formularz kontaktowy, dzięki któremu wiadomości trafiają bezpośrednio na moją skrzynkę e-mail. Dzięki lekkiemu i dynamicznemu interfejsowi użytkownik może wygodnie zapoznać się z moimi realizacjami.",
+      "Moje portfolio to nowoczesna i przejrzysta strona, która w prosty sposób prezentuje moje projekty. Stworzyłem ją przy użyciu Reacta, Motion, Tailwind CSS, React Scroll oraz EmailJS, co zapewnia płynne animacje, elegancki wygląd i intuicyjną nawigację.",
     link: "#",
     repoLink: "https://github.com/WasiakKacper/Portfolio",
   },
@@ -40,7 +40,7 @@ export const projectsData = [
   {
     url: "/Images/weather.png",
     description:
-      "Aplikacja pogodowa to intuicyjne narzędzie umożliwiające sprawdzenie aktualnej pogody w dowolnym mieście podanym przez użytkownika. Została stworzona w React i CSS.Dane o pogodzie, takie jak temperatura, wilgotność czy warunki atmosferyczne, pobierane są w czasie rzeczywistym dzięki integracji z OpenWeather API. Aplikacja umożliwia szybkie wyszukiwanie lokalizacji oraz prezentuje wyniki w przejrzysty i estetyczny sposób",
+      "Aplikacja pogodowa to intuicyjne narzędzie umożliwiające sprawdzenie aktualnej pogody w dowolnym mieście podanym przez użytkownika. Została stworzona w React i CSS.Dane o pogodzie, takie jak temperatura, wilgotność czy warunki atmosferyczne, pobierane są w czasie rzeczywistym dzięki integracji z OpenWeather API.",
     link: "https://weather-kacperwasiak.netlify.app/",
     repoLink: "https://github.com/WasiakKacper/Weather",
   },
